e6cd8fab00ad81a8acdbb59c5dc2100cacddc899
S13V3rt <strelchenko.dev@gmail.com>
2022-01-03 08:48:45 +0200

    f

--- .history/src/components/ImageItem_20220102212723.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="img-item">
-        <img src="{{ title }}" alt="{{ title }}" class="img-item__src" />
-        <span>{{ titel }}</span>
-    </div>
-</template>
--- .history/src/components/ImageItem_20220102212749.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="img-item">
-        <img src="{{ title }}" alt="{{ title }}" class="img-item__src" />
-        <h2 class="img-item__title">{{ titel }}</h2>
-    </div>
-</template>
